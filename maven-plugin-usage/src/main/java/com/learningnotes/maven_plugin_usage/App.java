32ddd01cb1fc2dead6d2f2beaf56e5c18d20e8ab
Prabu Ponnusamy <prabu.mponnusamy@gmail.com>
2022-05-02 22:58:23 +1200

    First maven plugins

---
+++ maven-plugin-usage/src/main/java/com/learningnotes/maven_plugin_usage/App.java
@@ -0,0 +1,13 @@
+package com.learningnotes.maven_plugin_usage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ maven-plugin-usage/src/test/java/com/learningnotes/maven_plugin_usage/AppTest.java
@@ -0,0 +1,20 @@
+package com.learningnotes.maven_plugin_usage;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
